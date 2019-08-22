169ddd5424adf81cb7cf8b16366d23eba277e604
kel <akyl_0221@mail.ru>
2019-08-22 08:10:46 +0600

    final try

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.6
+
+ENV PYTHONUNBUFFERED 1
+
+RUN mkdir /conv
+
+WORKDIR /conv
+
+ADD . /conv/
+
+RUN pip install --upgrade pip
+RUN pip install -U pipenv
+RUN pipenv install --system
+RUN pip install -r requirements.txt
--- freelance/settings.py
+++ freelance/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'freelance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('NAME'),
+        'HOST': config('HOST'),
+        'USER': config('USER_DB'),
+        'PORT': '3306',
+        'PASSWORD': config('PASSWORD'),
     }
 }
 
